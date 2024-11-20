4a97f47b5c7bbd320da1d194a9b9686b9638a022
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-11-20 20:26:45 +0700

    Membuat Column dengan ukuran fillMaxSize dan warna background dari resource color.

--- app/src/main/java/com/example/application6/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/application6/ui/screen/RencanaStudyView.kt
@@ -1,10 +1,17 @@
 package com.example.application6.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.application6.R
 import com.example.application6.model.Mahasiswa
 
 @Composable
@@ -24,4 +31,11 @@ fun RencanaStudyView(
         mutableStateOf("")
     }
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ){
+
+    }
 }
