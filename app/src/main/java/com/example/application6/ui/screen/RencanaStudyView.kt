918956a5b82fc9becfd09ed99ba9922917b7a81c
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-11-20 20:30:35 +0700

    Membuat Row di dalam Column dengan ukuran fillMaxWidth dan padding serta verticalAlignment center.

--- app/src/main/java/com/example/application6/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/application6/ui/screen/RencanaStudyView.kt
@@ -2,15 +2,19 @@ package com.example.application6.ui.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.example.application6.R
 import com.example.application6.model.Mahasiswa
 
@@ -36,6 +40,12 @@ fun RencanaStudyView(
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary))
     ){
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp), verticalAlignment = Alignment.CenterVertically
+        ){
 
+        }
     }
 }
