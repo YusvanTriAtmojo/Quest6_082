104089be5e10e94a02f91b9b958533b9c2cbf25d
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-11-21 09:25:15 +0700

    Memperbaiki navController, membuat variabel mahasiswaUiState, dan membuat komponen navigasi NavHost

--- app/src/main/java/com/example/application6/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/application6/navigation/PengelolaHalaman.kt
@@ -1,9 +1,12 @@
 package com.example.application6.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.application6.ui.viewmodel.MahasiswaViewModel
 import com.example.application6.ui.viewmodel.RencanaStudyViewModel
@@ -19,7 +22,13 @@ fun MahasiswaApp(
     modifier: Modifier = Modifier,
     mahasiswaViewModel: MahasiswaViewModel = viewModel(),
     krsViewModel: RencanaStudyViewModel = viewModel(),
-    navHostController: NavHostController = rememberNavController()
+    navController: NavHostController = rememberNavController()
 ){
-
+    val mahasiswaUiState = mahasiswaViewModel.mahasiswaUiState.collectAsState().value
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = Modifier.padding()
+    ) {
+    }
 }
