6ee488093c8f5d926f938e2a317cf81ff12f35c4
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-11-21 09:12:28 +0700

    Membuat File PengelolaHalaman yang di dalamnya dibuat enumerasi class Halaman yang isinya Splash, Mahasiswa, Matakuliah, dan Tampil. Lalu, membuat fungsi MahasiswaApp dengan parameternya.

---
+++ app/src/main/java/com/example/application6/navigation/PengelolaHalaman.kt
@@ -0,0 +1,25 @@
+package com.example.application6.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.application6.ui.viewmodel.MahasiswaViewModel
+import com.example.application6.ui.viewmodel.RencanaStudyViewModel
+
+enum class Halaman {
+    Splash,
+    Mahasiswa,
+    Matakuliah,
+    Tampil
+}
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navHostController: NavHostController = rememberNavController()
+){
+
+}
