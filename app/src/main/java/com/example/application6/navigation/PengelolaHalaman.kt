5365e49ee21e848ee59872f9883fec30b87fe53c
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-11-22 11:27:04 +0700

    Membuat Composable untuk routing halaman SplashView dengan navigasi button ke halaman Mahasiswa

--- app/src/main/java/com/example/application6/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/application6/navigation/PengelolaHalaman.kt
@@ -4,10 +4,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.application6.model.Mahasiswa
+import com.example.application6.ui.screen.DetailView
+import com.example.application6.ui.screen.MahasiswaFormView
+import com.example.application6.ui.screen.RencanaStudyView
+import com.example.application6.ui.screen.SplashView
 import com.example.application6.ui.viewmodel.MahasiswaViewModel
 import com.example.application6.ui.viewmodel.RencanaStudyViewModel
 
@@ -30,5 +37,12 @@ fun MahasiswaApp(
         startDestination = Halaman.Splash.name,
         modifier = Modifier.padding()
     ) {
+        composable(route = Halaman.Splash.name) {
+            SplashView(onMulaiBotton =  {
+                navController.navigate(
+                    Halaman.Mahasiswa.name
+                )
+            })
+        }
     }
 }
