e35cec3926a273c303eab1d2122e86aef1cb8f93
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-11-22 11:01:51 +0700

    Membuat Column ukuran fillMaxSize dengan background color dari resource berupa primary. Lalu, menambahkan Row dengan ukuran fillMaxWidth dan padding 25.dp

--- app/src/main/java/com/example/application6/ui/screen/DetailView.kt
+++ app/src/main/java/com/example/application6/ui/screen/DetailView.kt
@@ -1,9 +1,31 @@
 package com.example.application6.ui.screen
 
-
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.application6.R
 import com.example.application6.model.Mahasiswa
 import com.example.application6.model.RencanaStudy
 
@@ -20,4 +42,18 @@ fun DetailView(
         Pair("Matakuliah", uiStateDetailMatakuliah.mataKuliah),
         Pair("Kelas", uiStateDetailMatakuliah.kelas),
     )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary)),
+        horizontalAlignment = Alignment.CenterHorizontally, ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp)
+            ,verticalAlignment = Alignment.CenterVertically
+        ){
+
+        }
+    }
 }
